ac7a607b94cc0f2aca3267d503e32c352ddbf3cb
Gabriel G. Parrott <ganiparrott@gmail.com>
2020-11-06 20:28:22 -0800

    fixed the prompts. added README

--- test.py
+++ test.py
@@ -8,7 +8,7 @@
 from game import Game
 
 field_size = int(input("Please enter field size: "))
-wall_density = float(input("Please enter wall density: "))
+wall_density = float(input("Please enter wall density (0 <= wall_density <= 1): "))
 movement_per_reinforcement = float(input("Please enter number of movements per wave of reinforcements: "))
 
 game = Game(field_size, wall_density, movement_per_reinforcement)
